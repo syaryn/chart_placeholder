a31d2e21417ddf74b843e724ded4ee418bf6e72f
syaryn <blowfish448b@gmail.com>
2026-01-18 11:10:28 +0900

    Update /help test

--- main_test.ts
+++ main_test.ts
@@ -6,7 +6,7 @@ Deno.test("GET / returns HTML", async () => {
   assertEquals(res.status, 200);
 });
 
-Deno.test("GET /help returns HTML snippet", async () => {
+Deno.test("GET /help returns 404", async () => {
   const res = await app.request("http://localhost/help");
-  assertEquals(res.status, 200);
+  assertEquals(res.status, 404);
 });
